c9718dfac491d8ca4209d6a5a586de5ffadec747
philip.louw <philip.louw@standardbank.co.za>
2021-05-21 15:03:39 +0200

    Initial

--- README.md
+++ README.md
@@ -1,22 +1,15 @@
-chi-logger
+chi-zap-ecs-logger
 ===
-`chi-logger` is a simple logging middleware for [Chi](https://github.com/go-chi/chi) with support for `Zap` and `Logrus`
+forked from github.com/766b/chi-logger  
+
+`chi-zap-ecs-logger` is a simple logging middleware for [Chi](https://github.com/go-chi/chi) with support for `Zap` 
+Changed from 766b/chi-logger to use ecs defined fields, logrus removed
+
 
 Installation
 ---
 
-    go get github.com/766b/chi-logger
-
-Usage with Logrus
----
-
-    logger := logrus.New()
-    
-    r := chi.NewRouter()
-    r.Use(middleware.RequestID)
-    r.Use(middleware.RealIP)
-    r.Use(chilogger.NewLogrusMiddleware("router", logger))
-    ...
+    go get github.com/igknot/chi-zap-ecs-logger
 
 Usage with Zap
 ---
--- middleware.go
+++ middleware.go
@@ -1,13 +1,10 @@
 package chilogger
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
 	"go.uber.org/zap/zapcore"
-
-	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi/middleware"
 	"go.uber.org/zap"
@@ -15,17 +12,10 @@ import (
 
 type chilogger struct {
 	logZ *zap.Logger
-	logL *logrus.Logger
 	name string
 }
 
-// NewLogrusMiddleware returns a new Logrus Middleware handler.
-func NewLogrusMiddleware(name string, logger *logrus.Logger) func(next http.Handler) http.Handler {
-	return chilogger{
-		logL: logger,
-		name: name,
-	}.middleware
-}
+
 
 // NewZapMiddleware returns a new Zap Middleware handler.
 func NewZapMiddleware(name string, logger *zap.Logger) func(next http.Handler) http.Handler {
@@ -49,32 +39,18 @@ func (c chilogger) middleware(next http.Handler) http.Handler {
 
 		if c.logZ != nil {
 			fields := []zapcore.Field{
-				zap.Int("status", ww.Status()),
-				zap.Duration("took", latency),
-				zap.Int64(fmt.Sprintf("measure#%s.latency", c.name), latency.Nanoseconds()),
-				zap.String("remote", r.RemoteAddr),
-				zap.String("request", r.RequestURI),
-				zap.String("method", r.Method),
+				zap.Int("http.response.status_code", ww.Status()),
+				zap.Duration("event.duration", latency),
+				zap.String("client.address", r.RemoteAddr),
+				zap.String("url.path", r.RequestURI),
+				zap.String("http.request.method", r.Method),
 			}
 			if requestID != "" {
-				fields = append(fields, zap.String("request-id", requestID))
+				fields = append(fields, zap.String("http.request.id", requestID))
 			}
 			c.logZ.Info("request completed", fields...)
 		}
-		if c.logL != nil {
-			fields := logrus.Fields{
-				"status": ww.Status(),
-				"took":   latency,
-				fmt.Sprintf("measure#%s.latency", c.name): latency.Nanoseconds(),
-				"remote":  r.RemoteAddr,
-				"request": r.RequestURI,
-				"method":  r.Method,
-			}
-			if requestID != "" {
-				fields["request-id"] = requestID
-			}
-			c.logL.WithFields(fields).Info("request completed")
-		}
+
 	}
 	return http.HandlerFunc(fn)
 }
